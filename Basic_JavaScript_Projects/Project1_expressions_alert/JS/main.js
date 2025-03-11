6fbc998d3fa53736b90ad3f4536f1dd554f59180
NawalOmari <nawal.omari7@gmail.com>
2025-03-10 22:59:32 -0400

    Basic JavaScript Projects Assignment

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,5 +1,11 @@
-var Family = "The Hnachis", dad = "Mohammed", mom="Nawal", son="Mohamed Taha", daughter1="Noha", daughter2="Alae";
-document.write(son);
-document.write(daughter1);
-document.write(daughter2);
+//main.js
 
+window   .alert("Hello, World!");//window alert method
+
+document.write("For the last time, Hello, World!");//window write method
+
+var Sent1 = "This is the beginning of the string";//first variable
+var Sent2 = "This is the end of the string"//secound variable
+document.write(Sent1 + Sent2);//concatenation of the two variables
+
+document.write(33*84);//multiplication of two numbers
