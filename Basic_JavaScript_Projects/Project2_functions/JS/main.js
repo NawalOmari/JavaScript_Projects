5a141ba56dd655cd39b49219afca2497d2957dfd
NawalOmari <nawal.omari7@gmail.com>
2025-03-10 23:15:57 -0400

    Project2_Functions Assignment

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -7,3 +7,9 @@ function updateText() { // Assign two variables
     // Get the element by ID and update its content
     document.getElementById("info").innerHTML = `Name: ${name}, Age: ${age}`;
 }
+
+function myFunsction () { // Function that use += operator
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence; // Update the element with the concatenated string
+}
