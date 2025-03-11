17acdbf65658560dcebe81013f253c59653e8fea
NawalOmari <nawal.omari7@gmail.com>
2025-03-10 22:31:23 -0400

    Concatenating a String

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,3 @@
-document.write("Hello! \"Are you ready to learn?\"");
+document.write("\Be who you are and say what you feel,"
+    + " because those who mind don't matter and those who matter don't mind."
+    + "-Dr. Seuss");
