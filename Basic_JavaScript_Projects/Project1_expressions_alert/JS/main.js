68868f745a9bd34573558a78c40473c34241ae0b
NawalOmari <nawal.omari7@gmail.com>
2025-03-10 22:40:23 -0400

    Multiple Variables Assignment

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,2 +1,5 @@
-var B = "Concatenated" + " String";
-document.write(B);
+var Family = "The Hnachis", dad = "Mohammed", mom="Nawal", son="Mohamed Taha", daughter1="Noha", daughter2="Alae";
+document.write(son);
+document.write(daughter1);
+document.write(daughter2);
+
