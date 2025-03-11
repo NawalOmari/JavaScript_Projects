86186d9d310af547e1654eee7697751295a463f1
NawalOmari <nawal.omari7@gmail.com>
2025-03-10 23:06:15 -0400

    Function Assignment

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,9 @@
+// main.js
+
+function updateText() { // Assign two variables
+    let name = "Nawal";
+    let age = 41;
+
+    // Get the element by ID and update its content
+    document.getElementById("info").innerHTML = `Name: ${name}, Age: ${age}`;
+}
