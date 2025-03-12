1fc850db333b1e391c8ae7a4b388a0872262ca61
NawalOmari <nawal.omari7@gmail.com>
2025-03-12 02:15:10 -0400

    Type Of Operator Assignment

---
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,5 @@
+document.write(typeof "World!"); // Output: number
+document.write("<br>");
+document.write(typeof 3); // Output: number
+document.write("<br>");
+document.write("10"+5); // Output: 105
