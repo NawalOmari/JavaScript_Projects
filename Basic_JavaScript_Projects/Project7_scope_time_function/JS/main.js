b6a56166170c3315349ea08edf4c8f79c9e45f41
NawalOmari <nawal.omari7@gmail.com>
2025-03-12 04:00:48 -0400

    Else Assignment

--- Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
+++ Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
@@ -12,19 +12,39 @@ function Add_numbers_3() {
     var Y=10;
     document.write(20+Y+"<br>");
 }
-function Add_numbers_4() { 
-    document.write(Y+100);
-}
+//function Add_numbers_4() { 
+  //  document.write(Y+100);
+//}
 Add_numbers_3();
-Add_numbers_4();
+//Add_numbers_4();
 document.write("<br>");
 function Add_numbers_5() {
     var Z=10;
     console.log(20+Z);
 }
-function Add_numbers_6() {
-    console.log(Z+100);
-}
+//function Add_numbers_6() {
+  //  console.log(Z+100);
+//}
 Add_numbers_5();
-Add_numbers_6();
+//Add_numbers_6();
 document.write("<br>");
+if (1<2) {
+    document.write("The left number is smaller than the number on the right.");
+}
+document.write("<br>");
+function get_Date() {
+    if (new Date().getHours()<18) {
+        document.getElementById("Greeting").innerHTML="How are you today?";
+    }
+}
+function Age_Function() {
+    Age=document.getElementById("Age").value;
+    if (Age>=18) {
+        Vote="You are old enough to vote!";
+    }
+    else {
+        Vote="You are not old enough to vote!";
+    }
+    document.getElementById("How_old_are_you?").innerHTML=Vote;
+}
+
