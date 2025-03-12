e5259c971608d539a637fedbf49a8b35d846a64a
NawalOmari <nawal.omari7@gmail.com>
2025-03-12 03:32:14 -0400

    Project7_scope_time_function

---
+++ Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,30 @@
+var X=10;
+function Add_numbers_1() {
+    document.write(20+X+"<br>");
+}
+function Add_numbers_2() {
+    document.write(X+100);
+}
+Add_numbers_1();
+Add_numbers_2();
+document.write("<br>");
+function Add_numbers_3() {
+    var Y=10;
+    document.write(20+Y+"<br>");
+}
+function Add_numbers_4() { 
+    document.write(Y+100);
+}
+Add_numbers_3();
+Add_numbers_4();
+document.write("<br>");
+function Add_numbers_5() {
+    var Z=10;
+    console.log(20+Z);
+}
+function Add_numbers_6() {
+    console.log(Z+100);
+}
+Add_numbers_5();
+Add_numbers_6();
+document.write("<br>");
