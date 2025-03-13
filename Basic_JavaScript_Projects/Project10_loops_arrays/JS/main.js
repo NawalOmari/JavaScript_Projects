c44fd3fd5415dae8d7a5a79e11dafd2d7f47a9cf
NawalOmari <nawal.omari7@gmail.com>
2025-03-12 23:30:57 -0400

    Loops Assignment

---
+++ Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,9 @@
+function Call_Loop(){
+    var Digit = "";
+    var x = 1;
+    while (x < 11){
+        Digit += "<br>" + x;
+        x++;
+    }
+    document.getElementById("Loop").innerHTML = Digit;
+}
